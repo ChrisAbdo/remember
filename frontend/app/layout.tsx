ebb1b67356eaf0c118b491ae6e4b2502d8442b67
Chris Abdo <chris.j.abdo@gmail.com>
2024-06-30 20:31:58 -0400

    prisma + auth setup

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { AuthProvider } from "@/lib/auth/auth-provider";
+import Navbar from "@/components/layout/navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AuthProvider>
+        <body className={inter.className}>
+          <Navbar />
+          {children}
+        </body>
+      </AuthProvider>
     </html>
   );
 }
