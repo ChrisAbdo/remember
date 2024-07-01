e6c2347b569135e32f97dff2c4dfbedb36054fc2
Chris Abdo <chris.j.abdo@gmail.com>
2024-06-30 21:49:25 -0400

    partially functional now lol

--- extension/background.js
+++ extension/background.js
@@ -1,29 +1,3 @@
-// chrome.contextMenus.onClicked.addListener((info, tab) => {
-//   if (info.menuItemId === "saveHighlightedText") {
-//     chrome.storage.sync.get("userId", ({ userId }) => {
-//       if (!userId) {
-//         console.error("User not authenticated");
-//         return;
-//       }
-//       const highlight = {
-//         text: info.selectionText,
-//         userId: userId,
-//         url: tab.url,
-//         title: tab.title,
-//       };
-
-//       fetch("https://your-web-app-url.com/api/save-highlight", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify(highlight),
-//       })
-//         .then((response) => response.json())
-//         .then((data) => console.log("Highlight saved:", data))
-//         .catch((error) => console.error("Error saving highlight:", error));
-//     });
-//   }
-// });
-
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "saveHighlightedText",
@@ -42,7 +16,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
       const highlight = {
         text: info.selectionText,
         userId: userId,
-        url: tab.url,
+        url: info.selectionText,
         title: tab.title,
       };
       //
